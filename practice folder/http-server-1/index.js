1591b563d83a536105a6be90f2ec13cc4ee703f4
ayush jiw <ayush9990@gmail.com>
2023-12-15 22:53:19 +0530

    adding hhtp assignments

--- practice folder/http-server-1/index.js
+++ practice folder/http-server-1/index.js
@@ -4,19 +4,57 @@ const app = express()
 const port = 3000
 app.use(bodyParser.json());
 
+const users = [
+  {
+    name : "John" , 
+    kidneys : [
+      {
+        healthy : false
+      },
+      {
+        healthy : true
+      }
+    ]
+  }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello Worlddddddd!')
+  const johnKidneys = users[0].kidneys ;
+  const numberOfKidneys = johnKidneys.length ;
+  let numHealthyKidneysArray =  johnKidneys.filter((kidney) => kidney.healthy == true);
+  let numHealthyKidneys = numHealthyKidneysArray.length ; 
+  let numUnhealthyKidneys = numberOfKidneys - numHealthyKidneys ; 
+  res.json(
+    {
+      numberOfKidneys,
+      numHealthyKidneys,
+      numUnhealthyKidneys
+    }
+  )
+
 })
 
-app.post('/bakend-api/conversation',(req,res) =>{
-    const message = req.body.message ; 
-    console.log(req.body);
-    console.log(message);
+app.post('/',(req,res) =>{
+    const isHealthy = req.body.isHealthy ;
+    users[0].kidneys.push({healthy : isHealthy});
+    console.log(users[0].kidneys)
     res.json(
-        {
-            output: "yo yo yo"
-        }
+      {
+        msg : "Done"
+      }
     )
+})
+
+app.put('/',(req,res) =>{
+  const johnKidneys = users[0].kidneys ;
+  for (i = 0 ; i < johnKidneys.length ; i++){
+    johnKidneys[i].healthy = true ;
+  }
+
+  res.json(
+    {
+    }
+  )
 })
 
 app.listen(port, () => {
